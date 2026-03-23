82b26e894fd61e8740fc09233785f715cf7bac56
Ashish Balwada <57625151+AshishBalwada22@users.noreply.github.com>
2026-03-23 13:40:15 +0000

    array

---
+++ DSA-200/rotateMatrix.cpp
@@ -0,0 +1,16 @@
+// https://takeuforward.org/plus/dsa/problems/rotate-matrix-by-90-degrees?subject=dsa-concept-revision
+
+class Solution {
+public:
+    void rotateMatrix(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
