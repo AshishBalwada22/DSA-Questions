0aa304ee154b7e8f7402ce3880efa88a71c94074
Ashish Balwada <57625151+AshishBalwada22@users.noreply.github.com>
2026-03-31 13:23:58 +0000

    stl

---
+++ CP/STL_question/LR_insertion.cpp
@@ -0,0 +1,33 @@
+// https://atcoder.jp/contests/abc237/tasks/abc237_d
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    deque<int> dq1,dq2;
+    int num = 0;
+    for(char ch:s){
+      if(ch=='L'){
+        dq2.push_front(num);
+      }else if(ch=='R'){
+        dq1.push_back(num);
+      }
+      num++;
+    }
+    for(auto val:dq1) cout<<val<<" ";
+    cout<<num<<" ";
+    for(auto val:dq2) cout<<val<<" ";
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int t=1;
+    while(t--){
+        solve();
+    }
+}
