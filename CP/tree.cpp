5e468004bdf3bc00f26307c1714f93462eeda3eb
Ashish Balwada <57625151+AshishBalwada22@users.noreply.github.com>
2026-04-24 16:06:17 +0000

    distance

--- CP/tree.cpp
+++ CP/tree.cpp
@@ -23,3 +23,34 @@ void solve(){
     dfs(1,-1);
 }
 
+// find the distance of all node from root node
+
+vector<vector<int>> edges;
+vector<int>dist;
+
+void dfs(int cur,int par){
+    if(par!=-1){
+        dist[cur]=dist[par]+1;
+    }
+    for(auto nxt:edges[cur]){
+        if(nxt!=par){
+            dfs(nxt,cur);
+        }
+    }
+}
+void solve(){
+    int n,e;
+    cin>>n>>e;
+    edges.resize(n+1);
+    dist.resize(n+1);
+    for(int i=0;i<e;i++){
+        int a,b;
+        cin>>a>>b;
+        edges[a].push_back(b);
+        edges[b].push_back(a);
+    }
+    dfs(1,-1);
+    for(int i=1;i<=n;i++){
+        cout<<dist[i]<<" ";
+    }
+}
