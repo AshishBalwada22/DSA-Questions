f69f1a50a26224e062496e588953a2dbb0bfcc93
Ashish Balwada <57625151+AshishBalwada22@users.noreply.github.com>
2026-04-24 20:19:26 +0000

    tree

--- CP/tree.cpp
+++ CP/tree.cpp
@@ -30,24 +30,31 @@
 4) find the centre of tree
 
 vector<vector<int>> edges;
+int n;
 vector<int>dist;
 vector<int>parent;
+vector<int>subsz;
+int ansp=0;
 
 void dfs(int cur,int par,int dd){
     dist[cur]=dd;
     parent[cur]=par;
+    subsz[cur]=1;// include myself
     for(auto nxt:edges[cur]){
         if(nxt!=par){
             dfs(nxt,cur,dd+1);
+            subsz[cur]+=subsz[nxt]; // add into myself after calculated for children
+            ansp+=subsz[nxt]*(n-subsz[nxt]);// this is for how many times edge will be counted or part of path, contribution technique
         }
     }
 }
 void solve(){
-    int n,e;
+    int e;
     cin>>n>>e;
     edges.resize(n+1);
     dist.resize(n+1);
     parent.resize(n+1);
+    subsz.resize(n+1);
     for(int i=0;i<e;i++){
         int a,b;
         cin>>a>>b;
@@ -111,4 +118,39 @@ void solve(){
         int cen2=parent[cen1];
         cout<<cen1<<" "<<cen2<<endl;
     }
+    // if start from anywhere and end anywhere then find the minimum number of edges traverse
+    // ans = 2*e - max_distance(x,y)
+    cout<<2*e-dist[y]<<endl;
+
+    // size of subtree
+    // we need vector for storing subtree size
+    ansp=0;
+    dfs(1,-1,0);
+    for(int i=1;i<=n;i++){
+        cout<<subsz[i]<<" ";
+    }
+    cout<<endl;
+
+    // find the sum of distance of all pair of node
+    cout<<ansp<<endl;
 }
+
+5) start from anywhere and end anywhere, visit each node exactly once. find the minimum no. of edges traverse
+- then we need to traverse from two farthest node so that number of edges traverse will be minimum
+- if start and end at same point then this is called euler tour
+- if n nodes, (n-1) edges => (n-1)*2 edges traverse in euler tour
+- if we have two farthest node, then i will go to second node and then come back to same starting node
+- so path = 2*(n-1) - distance(x,y) = 2E-D
+- if want to minimize the path, then distance(x,y) would be maximum, so we need to find two farthest node
+
+6) find the size of subtree of each node
+
+7) centroid of tree - if we remove a particular node, then size of each component should be less than or equal to n/2
+- subsz[child]<=n/2
+
+8) contribution technique on tree
+find the sum of distance of all pair of node
+- calculate how many times any edges would be counted or how many path of edge will be part of
+- subsz[cur]*(total_nodes-subsz[cur])
+
+
