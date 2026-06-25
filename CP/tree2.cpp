b14472011b54dee6f7a558200a4a575751e70dce
Ashish Balwada <57625151+AshishBalwada22@users.noreply.github.com>
2026-06-25 15:23:39 +0000

    tree

--- CP/tree2.cpp
+++ CP/tree2.cpp
@@ -90,3 +90,61 @@ void solve(){
    }
 }
 
+3) Find the sum of distance of all pair of node
+f(u) = summation(dist(x,u)), 1<=x<=n, find f(u) for all node
+
+vector<vector<int>>edges;
+vector<int>distt;
+vector<int>subsz;
+vector<int>ans;
+int n;
+
+void dfs1(int cur, int par, int dis){ // calculate distance of root node to all node, also calculate the subsize(number of node in that subtree) of node in post order
+    distt[cur] = dis;
+    subsz[cur]=1;
+    for(auto nxt : edges[cur]){
+        if(nxt!=par){
+            dfs1(nxt,cur,dis+1);
+            subsz[cur]+=subsz[nxt];
+        }
+    } 
+}
+
+void dfs2(int cur, int par, int curans){ // re-rooting, let's say node x is a root and node y is a child, if we re-root x to y, then how many times the edge between x and y is counted now
+// number of node present in x subtree, for all those nodes, distance that is contributed to the answer would be incremented by 1 for all node, as root shifted to node y
+// number of node present in y subtree, for all those nodes, distance would be decremented by 1 for all node
+// ans[y] = ans[x] - subsize[y] + (n-subsize[y])
+    ans[cur]=curans;
+    for(auto nxt:edges[cur]){
+        if(nxt!=par){
+            dfs2(nxt,cur,curans-subsz[nxt]+(n-subsz[nxt]));
+        }
+    }
+}
+
+void solve(){
+    int edge;
+    cin>>n>>edge;
+    edges.resize(n+1);
+    distt.resize(n+1,0);
+    subsz.resize(n+1,0);
+    ans.resize(n+1,0);
+    for(int i=0;i<edge;i++){
+        int a,b;
+        cin>>a>>b;
+        edges[a].push_back(b);
+        edges[b].push_back(a);
+    }
+    dfs1(1,0,0);
+    int anss = 0;
+    for(int i=1;i<=n;i++){
+        anss+=distt[i];
+    }
+    dfs2(1,0,anss);
+
+    for(int i=1;i<=n;i++){
+        cout<<ans[i]<<" ";
+    }
+}
+
+
